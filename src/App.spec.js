34bbc8f8ad014efe4980610f20b0ec9fe5e95d1a
Matthew Lockdall <mlockdall26@gmail.com>
2019-11-11 17:42:41 -0600

    wrote basic unit test using jest and renderer

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,6 +1,58 @@
-describe('My Test Suite', () => {
-  it('My Test Case', () => {
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./App";
+import Counter from "./components/Counter";
+import dataReducer from "./reducers/dataReducer";
+
+const list = ["a", "b", "c"];
+
+describe("My Test Suite", () => {
+  it("My Test Case", () => {
     expect(true).toEqual(true);
   });
+});
 
+describe("Counter", () => {
+  test("snapshot renders", () => {
+    const component = renderer.create(<Counter counter={1} />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("App", () => {
+  test("snapshot renders", () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe("App", () => {
+  describe("Reducer", () => {
+    it("should set a list", () => {
+      const state = { list: [], error: null };
+      const newState = dataReducer(state, {
+        type: "SET_LIST",
+        list
+      });
+      expect(newState).toEqual({ list, error: null });
+    });
+    it("should reset the error if list is set", () => {
+      const state = { list: [], error: true };
+      const newState = dataReducer(state, {
+        type: "SET_LIST",
+        list
+      });
+      expect(newState).toEqual({ list, error: null });
+    });
+    it("should set the error", () => {
+      const state = { list: [], error: null };
+      const newState = dataReducer(state, {
+        type: "SET_ERROR",
+        list
+      });
+      expect(newState).toBeTruthy();
+    });
+  });
 });
